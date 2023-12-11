06e054f33c6bbf2bc621b7533859d2a8ef64db55
Lei Xu <komushi@gmail.com>
2023-12-12 01:03:20 +0900

    WIP deviceReg with listing internalName

--- src/handler/routes.js
+++ src/handler/routes.js
@@ -85,7 +85,7 @@ router.post('/deviceReg', async (req, res) => {
     console.log('routes.deviceReg roomCodeObj:' + JSON.stringify(roomCodeObj));
 
     internalNames.forEach(internalName => {
-      const listing = await getListing(internalName);
+      const listing = await storage.getListing(internalName);
 
       let listingId;
       if (!listing) {
